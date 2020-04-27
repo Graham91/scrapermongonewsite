bae560938fff52622de472d32f0a2b787dc60386
grambo <gramham@live.com>
2020-04-27 15:21:32 -0600

    added basic file structure

---
+++ public/assets/scripts/index.js
@@ -0,0 +1,57 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+    $(".eatit").on("click", function(event) {
+      var id = $(this).data("id");
+      var newdev = $(this).data("newdev");
+  
+      var newdevState = {
+        devoured: newdev
+      };
+  
+      // Send the PUT request.
+      $.ajax("/api/updateburger/" + id, {
+        type: "PUT",
+        data: newdevState
+      }).then(
+        function() {
+         
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  $(".deleteit").on("click", function(event){
+    var id = $(this).data("id");
+
+
+   $.ajax("/api/deleteburger/" + id, {
+    type: "DELETE"
+  }).then(
+    function() {
+     
+      // Reload the page to get the updated list
+      location.reload();
+    }
+  );
+
+  });
+    $(".burger-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+  
+      var newburger = {
+        name: $("#burger").val().trim()
+      };
+  
+      // Send the POST request.
+      $.ajax("/api/addburger", {
+        type: "POST",
+        data: newburger
+      }).then(
+        function() {
+
+          location.reload();
+        }
+      );
+    });
+  });
